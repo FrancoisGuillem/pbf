a20ba1734c97821c56364e033457e9346a50bd62
cuche27 <guillem_francois@yahoo.fr>
2020-02-22 14:44:51 +0100

    Page liste des participants

--- wordpress/wp-content/themes/pbf/archive-participant.php
+++ wordpress/wp-content/themes/pbf/archive-participant.php
@@ -8,41 +8,86 @@
  */
 
 get_header(); ?>
+	<style media="screen">
+		.participant {
+			position: relative;
+			margin-top: 20px;
+			padding: 20px;
+		}
 
-	<section id="primary" class="content-area col-sm-12 col-lg-8">
+		.participant img {
+			transition: all .2s ease-in-out;
+		}
+
+		.participant:hover img {
+			transform: scale(1.3);
+			filter: sepia(50%);
+			-webkit-filter: sepia(50%);
+			-moz-filter: sepia(50%);
+		}
+
+		.participant .thumbnail-container {
+			overflow: hidden;
+		}
+
+		.participant .participant-thumbnail {
+			margin: 0;
+		}
+
+		.participant-preview-title {
+			position:absolute;
+			bottom: 0px;
+			right: 0px;
+			margin:0 20px;
+			padding: 10px;
+			width: 75%;
+			background-color: rgba(255, 255, 255, 0.85);
+			text-align: center;
+		}
+
+		.participant .participant-preview-title a {
+			color:  #1c1c24;
+			font-size: 24px;
+		}
+
+		.participant:hover .participant-preview-title a {
+			color: #ff007a;
+		}
+	</style>
+	<section id="primary" class="content-area col-sm-12 col-lg-12">
 		<main id="main" class="site-main" role="main">
 
 		<?php
 		if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				'<h1 class="page-title"><?= _e("Les Participants du Paris Beer Festival", "pbf") ?></h1>'
+				<h1 class="page-title"><?= _e("Les Participants du Paris Beer Festival", "pbf") ?></h1>
 				<?php
 					the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
+			<div class="row">
+				<?php
+				/* Start the Loop */
+				while ( have_posts() ) : the_post();
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+					/*
+					 * Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'template-parts/content-participant-preview' );
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content-participant', get_post_format() );
+				endwhile;
 
-			endwhile;
+				the_posts_navigation();
 
-			the_posts_navigation();
+			else :
 
-		else :
+				get_template_part( 'template-parts/content_participant-preview');
 
-			get_template_part( 'template-parts/content_participant', 'none' );
-
-		endif; ?>
-
+			endif; ?>
+			</div>
 		</main><!-- #main -->
 	</section><!-- #primary -->
 
---
+++ wordpress/wp-content/themes/pbf/template-parts/content-participant-preview.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package pbf
+ */
+
+$metadata = get_post_meta(get_the_ID());
+$events = $metadata["events"][0];
+?>
+
+<div class="col-lg-3 col-md-4 participant" id="post-<?php the_ID(); ?>">
+	<a href="<?= esc_url( get_permalink() ) ?>">
+		<div class="thumbnail-container">
+			<div class="post-thumbnail participant-thumbnail">
+				<?php the_post_thumbnail(); ?>
+			</div>
+		</div>
+		<div class="participant-preview-title">
+			<?php the_title( '<h2><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
+		</div>
+	</a>
+</div>
