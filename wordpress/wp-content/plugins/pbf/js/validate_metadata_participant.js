e8fd369ac9a5debc01eeed7464122e800731246a
cuche27 <guillem_francois@yahoo.fr>
2020-03-16 09:07:56 +0100

    Une et une seule catégorie par participant

--- wordpress/wp-content/plugins/pbf/js/validate_metadata_participant.js
+++ wordpress/wp-content/plugins/pbf/js/validate_metadata_participant.js
@@ -1,8 +1,23 @@
 jQuery().ready(function($) {
+  $.validator.addMethod(
+    "exactly_one_category",
+    function(value, element) {
+      var categories = $("input[name='tax_input[participant_cat][]']:checked");
+      return categories.length == 0;
+    },
+    "Sélectionnez une et une seule catégorie"
+  );
+
+  $.validator.setDefaults({
+    ignore: [],
+    // any other default options and/or rules
+  });
+
   $("#post").validate({rules: {
     facebook: "url",
     instagram: "url",
-    address: "required"
+    address: "required",
+    "tax_input[participant_cat][]": "exactly_one_category"
   }});
 
 });
