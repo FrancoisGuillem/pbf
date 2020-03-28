208d6ccd5e99f5e019b7d116f5e87e68ada857c4
Thomas Beduneau <thomas@switch-company.com>
2020-03-28 21:30:07 +0100

    homepage - update how the sections are managed

--- wordpress/wp-content/themes/pbf/homepage.php
+++ wordpress/wp-content/themes/pbf/homepage.php
@@ -26,76 +26,29 @@ get_header(); ?>
     </div>
   </div>
 </div>
-<section class="festival-intro container">
-  <?php while (have_posts()) : the_post(); ?>
-    <h2 class="festival-title"><?php the_title(); ?></h2>
-    <?php the_content(); ?>
-  <?php endwhile; ?>
-  <div class="festival-links" data-bind="scroll-slider">
-    <div class="festival-links-wrapper">
-      <ul>
-        <li>
-          <a name="festival-link-2" class="card" href="/event">
-            <span class="card-image">
-              <img src="<?php echo get_template_directory_uri(); ?>/inc/assets/festival-cards/event.jpg" alt="" />
-            </span>
-            <span class="label"><?php _e("[:fr]Événements[:en]Events[:]"); ?></span>
-          </a>
-        </li>
-        <li>
-          <a name="festival-link-1" class="card" href="/participant">
-            <span class="card-image">
-              <img src="<?php echo get_template_directory_uri(); ?>/inc/assets/festival-cards/participant.jpg" alt="" />
-            </span>
-            <span class="label"><?php _e("[:fr]Participants[:en]Participants[:]"); ?></span>
-          </a>
-        </li>
-        <li>
-          <a name="festival-link-3" class="card" href="/">
-            <span class="card-image">
-              <img src="<?php echo get_template_directory_uri(); ?>/inc/assets/festival-cards/final.jpg" alt="" />
-            </span>
-            <span class="label"><?php _e("[:fr]Grand Final[:en]Grand Finale[:]"); ?></span>
-          </a>
-        </li>
-      </ul>
-    </div>
-    <ul class="festival-index" role="presentation">
-      <li class="current"></li>
-      <li></li>
-      <li></li>
-    </ul>
-  </div>
-</section>
+<?php
+$getchilds = array(
+  'parent'        => $post->ID,
+  'child_of'      => $post->ID,
+  'sort_column'   => 'menu_order',
+  'sort_order'    => 'ASC'
+);
 
-<section class="text-illus">
-  <div class="container">
-    <div class="text-illus-image">
-      <span class="image-wrapper">
+$postlist = get_pages($getchilds);
 
-        <img src="<?php echo get_template_directory_uri(); ?>/inc/assets/asso/asso@0.5x.jpg" srcset="<?php echo get_template_directory_uri(); ?>/inc/assets/asso/asso@0.33x.jpg 356w,
-                <?php echo get_template_directory_uri(); ?>/inc/assets/asso/asso@0.5x.jpg 540w,
-                <?php echo get_template_directory_uri(); ?>/inc/assets/asso/asso.jpg 1080w" sizes="(min-width: 992px) 540px, 356px" alt="" />
-      </span>
-    </div>
+foreach ($postlist as $post) {
 
-    <div class="text-illus-content">
-      <?php
-      $id = url_to_postid("/association");
-      $asso_desc = get_post($id);
-      if ($asso_desc) {
-        echo '<h2 class="text-illus-title">' . get_the_title($asso_desc) . '</h2>';
-        echo apply_filters('the_content', $asso_desc->post_content);
-      }
-      ?>
-      <p><a class="link-page" href="https://www.parisbeerclub.fr">
-          <?php get_template_part("inc/assets/arrow-right.svg"); ?>
-          <span><?= __("[:en]Read more[:][:fr]En savoir plus[:]") ?></span>
-        </a></p>
-    </div>
+  // setup post data, so page template will use it as a "master" post
+  setup_postdata($post);
 
-  </div>
-</section>
+  // we get page template name for the post and remove ".php" at the end to make it work
+  $template = preg_replace("/\.php$/", "", get_page_template_slug($post));
+
+  // now let WordPress fetch that page for you
+  echo get_template_part($template);
+}
+?>
+
 <section class="sponsors">
   <div class="container">
     <h2 class="sponsors-title"><?php _e("[:fr]Partenaires[:en]Partners[:]"); ?></h2>
---
+++ wordpress/wp-content/themes/pbf/section-image-links.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Template Name: Image links
+ */
+?>
+
+<section class="festival-intro container">
+  <h2 class="festival-title"><?php the_title(); ?></h2>
+  <?php the_content();
+  $metadata = get_post_meta($post->ID);
+  $category = $metadata["images-category"][0] ?? "";
+  if ($category) {
+  ?>
+    <div class="festival-links" data-bind="scroll-slider">
+      <div class="festival-links-wrapper">
+        <ul>
+          <?php
+          query_posts(array(
+            'category_name'  => $category,
+            'posts_per_page' => -1,
+            'order' => 'ASC'
+          ));
+          while (have_posts()) : the_post();
+            $cardMetadata = get_post_meta($post->ID);
+            $link = $cardMetadata["card-link"][0] ?? "";
+          ?>
+            <li>
+              <a class="card" href="<?= $link ?>">
+                <span class="card-image">
+                  <?php the_post_thumbnail(); ?>
+                </span>
+                <span class="label"><?php the_title(); ?></span>
+              </a>
+            </li>
+          <?php
+          endwhile;
+          ?>
+        </ul>
+      </div>
+      <ul class="festival-index" role="presentation">
+        <li class="current"></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  <?php } ?>
+</section>
