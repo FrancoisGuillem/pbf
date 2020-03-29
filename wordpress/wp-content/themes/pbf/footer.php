2d7d7b2012c31d9e81ee10cf1a799265a3c7a5b9
Thomas Beduneau <thomas@switch-company.com>
2020-03-29 14:55:26 +0200

    fix footer scripts

--- wordpress/wp-content/themes/pbf/footer.php
+++ wordpress/wp-content/themes/pbf/footer.php
@@ -67,7 +67,7 @@
 <?php endif; ?>
 </div>
 
-<script src="<?php echo get_template_directory_uri(); ?>/script.js" />
+<script src="<?php echo get_template_directory_uri(); ?>/script.js"></script>
 <?php wp_footer(); ?>
 </body>
 
