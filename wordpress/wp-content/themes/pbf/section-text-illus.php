6c57520376f1bbaf607bcfb40a7b0a72f070e5cc
Thomas Beduneau <thomas@switch-company.com>
2020-05-09 16:00:37 +0200

    text-illus - handle figcaption

--- wordpress/wp-content/themes/pbf/section-text-illus.php
+++ wordpress/wp-content/themes/pbf/section-text-illus.php
@@ -6,15 +6,21 @@
 ?>
 <section class="text-illus">
   <div class="container">
-    <div class="text-illus-image">
+    <figure class="text-illus-image">
       <span class="image-wrapper">
         <?php the_post_thumbnail([356, 540, 992]); ?>
       </span>
-    </div>
+      <?php
+      $caption = get_the_post_thumbnail_caption();
+
+      if ($caption) { ?>
+        <figcaption><?= qtranxf_useCurrentLanguageIfNotFoundUseDefaultLanguage(get_the_post_thumbnail_caption()); ?></figcaption>
+      <?php }
+      ?>
+    </figure>
 
     <div class="text-illus-content">
       <h2 class="text-illus-title"><?= get_the_title() ?></h2>
-
       <?= get_the_content_pbf(); ?>
     </div>
 
