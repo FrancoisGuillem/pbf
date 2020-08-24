e41642092badb183bc79176a3b75f41323d931cf
Thomas Beduneau <thomas@switch-company.com>
2020-08-24 16:40:25 +0200

    add events template for the weekend

--- wordpress/wp-content/themes/pbf/section-timeline.php
+++ wordpress/wp-content/themes/pbf/section-timeline.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Template Name: Laureates
+ * Template Name: Events
  */
 
 $titleLevel = wp_cache_get('titleLevel');
@@ -11,41 +11,49 @@ if ($titleLevel === false) {
 }
 
 $subtitle = get_post_meta(get_the_ID(), 'sub-title', true);
+$day = get_post_meta(get_the_ID(), 'event_day', true);
 
 ?>
-<section class="laureates container">
+<section class="timeline container">
   <?php if ($subtitle) { ?>
-    <header class="laureates-title">
+    <header class="timeline-title">
       <h<?= $titleLevel; ?>><?= get_the_title() ?></h<?= $titleLevel; ?>>
       <p role="doc-subtitle"><?= $subtitle ?></p>
     </header>
   <?php } else { ?>
-    <h<?= $titleLevel; ?> class="laureates-title"><?= get_the_title() ?></h<?= $titleLevel; ?>>
+    <h<?= $titleLevel; ?> class="timeline-title"><?= get_the_title() ?></h<?= $titleLevel; ?>>
   <?php } ?>
   <?php if (!empty(get_the_content())) { ?>
-    <div class="laureate-content">
+    <div class="timeline-content">
       <?= get_the_content_pbf(); ?>
     </div>
   <?php } ?>
   <ul class="timeline-list">
     <?php
     query_posts(array(
-      'category_name'  => 'laureate',
+      'category_name'  => 'event',
       'posts_per_page' => -1,
-      'meta_key' => 'laureate-year',
+      'meta_key' => 'event_time',
       'orderby' => 'meta_value_num',
-      'order' => 'ASC'
+      'order' => 'ASC',
+      'meta_query' => array(
+        array(
+          'key' => 'event_day',
+          'value' => $day,
+          'compare' => '=',
+          'type' => 'STRING'
+        )
+      )
     ));
     while (have_posts()) : the_post();
-      $year = get_post_meta(get_the_ID(), 'laureate-year', true);
+      $time = get_post_meta(get_the_ID(), 'event_time', true);
     ?>
 
       <li class="timeline-entry">
         <div class="header">
-          <span class="image-wrapper timeline-entry-image"><?php the_post_thumbnail([80, 80]); ?></span>
           <div class="title">
             <p class="timeline-entry-title"><?php the_title(); ?></p>
-            <p class="timeline-entry-time"><?= $year ?></p>
+            <p class="timeline-entry-time"><?= $time ?></p>
           </div>
         </div class="header">
         <?= the_content(); ?>
